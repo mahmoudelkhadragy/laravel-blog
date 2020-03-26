0c1a7a146846e08055cdbbcaf98cae80c660de16
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-03-26 23:35:38 +0200

    mahmoud

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mahmoud elkhadragy
                 </div>
 
                 <div class="links">
